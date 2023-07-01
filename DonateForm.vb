f08aee57f4a1741d0256adb475346bd45abd4f94
CJ Seven <hperm9166@gmail.com>
2023-07-02 00:33:24 +0800

    Sorted the dynamically generated items according to distance ascending order

--- DonateForm.vb
+++ DonateForm.vb
@@ -60,6 +60,19 @@ Public Class DonateForm
         Return deg * (Math.PI / 180)
     End Function
 
+    ' Create a class to store the request data and its distance
+    Private Class RequestData
+        Public Property RequestID As Integer
+        Public Property Title As String
+        Public Property Description As String
+        Public Property Location As String
+        Public Property Latitude As Double
+        Public Property Longitude As Double
+        Public Property Pax As Integer
+        Public Property Status As String
+        Public Property Distance As Double
+    End Class
+
     Public Sub DonateFormRefreshData()
         If connect.State = ConnectionState.Closed Then
             connect.Open()
@@ -83,7 +96,8 @@ Public Class DonateForm
             reader1.Close()
         End If
 
-
+        ' Create a list to store the request data with distance
+        Dim requestDataList As New List(Of RequestData)()
 
         'Retrieve data from database
         sql = "SELECT RequestID, title, description, location, latitude, longitude, pax, status FROM Requests"
@@ -106,40 +120,6 @@ Public Class DonateForm
         While reader.Read()
             Dim status As Object = reader("status")
             If status Is DBNull.Value Then ' Check if the status column is null
-                Dim customContainer As Panel = New Panel()
-                customContainer.BorderStyle = BorderStyle.FixedSingle
-                customContainer.Width = 300
-                customContainer.Height = 180
-                customContainer.BackColor = System.Drawing.Color.FromArgb(255, 255, 255)
-                customContainer.BorderStyle = BorderStyle.None
-
-                ' Set the column and row location of the custom container
-                table.SetColumn(customContainer, col)
-                table.SetRow(customContainer, row)
-
-                Dim titleLabel As Label = New Label()
-                titleLabel.Text = reader("title").ToString()
-                titleLabel.Location = New Point(10, 10)
-                titleLabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-                titleLabel.Name = "lblTitle"
-                titleLabel.Size = New System.Drawing.Size(250, 20)
-                titleLabel.AutoEllipsis = True
-
-                Dim descriptionLabel As Label = New Label()
-                descriptionLabel.Text = "Description: " & reader("description").ToString()
-                descriptionLabel.Location = New Point(10, 30)
-                descriptionLabel.AutoEllipsis = True
-                descriptionLabel.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-                descriptionLabel.Name = "lblDescription"
-                descriptionLabel.Size = New System.Drawing.Size(250, 60)
-
-                Dim paxLabel As Label = New Label()
-                paxLabel.Text = "Pax: " & reader("pax").ToString()
-                paxLabel.Location = New Point(10, 90)
-                paxLabel.AutoSize = True
-                paxLabel.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-                paxLabel.Name = "lblPax"
-
 
                 ' Retrieve request information from the database
                 Dim latitude As Double = CDbl(reader("latitude"))
@@ -147,59 +127,116 @@ Public Class DonateForm
                 ' Calculate the distance using the Haversine formula
                 Dim distance As Double = CalculateDistance(userLatitude, userLongitude, latitude, longitude)
                 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-                Dim distanceLabel As Label = New Label()
-                distanceLabel.Text = "Distance: " & distance.ToString("F2") & " km from you"
-                distanceLabel.Location = New Point(10, 110)
-                distanceLabel.AutoSize = True
-                distanceLabel.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-                distanceLabel.Name = "lblPax"
 
+                ' Create a new RequestData object and populate it with the request information
+                Dim requestData As New RequestData() With {
+                        .RequestID = CInt(reader("RequestID")),
+                        .Title = reader("title").ToString(),
+                        .Description = reader("description").ToString(),
+                        .Location = reader("location").ToString(),
+                        .Latitude = latitude,
+                        .Longitude = longitude,
+                        .Pax = CInt(reader("pax")),
+                        .Status = reader("status").ToString(),
+                        .Distance = distance
+                    }
 
-
-                Dim button As KryptonButton = New KryptonButton()
-                button.Values.Text = "View"
-
-                button.Location = New System.Drawing.Point(185, 135)
-                button.Name = "btnView"
-                button.OverrideDefault.Back.Color1 = System.Drawing.Color.Gold
-                button.OverrideDefault.Back.Color2 = System.Drawing.Color.Gold
-                button.Size = New System.Drawing.Size(100, 30)
-                button.StateCommon.Back.Color1 = System.Drawing.Color.Gold
-                button.StateCommon.Back.Color2 = System.Drawing.Color.Gold
-                button.StateCommon.Border.DrawBorders = CType((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top Or ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) _
-                Or ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) _
-                Or ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right), ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)
-                button.StateCommon.Border.Rounding = 10
-                button.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.Black
-                button.StateCommon.Content.ShortText.Color2 = System.Drawing.Color.Black
-                button.StateCommon.Content.ShortText.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-
-                'Get request ID and handles the view button
-                Dim requestID As Integer = reader.GetInt32(reader.GetOrdinal("RequestID"))
-                button.Tag = requestID
-                AddHandler button.Click, AddressOf button_Click
-
-
-                customContainer.Controls.Add(titleLabel)
-                customContainer.Controls.Add(descriptionLabel)
-                customContainer.Controls.Add(paxLabel)
-                customContainer.Controls.Add(distanceLabel)
-                customContainer.Controls.Add(button)
-
-
-                ' Add the custom container to the form or container control
-
-                table.Controls.Add(customContainer)
-
-                col += 1 ' Move to the next column
-
-                ' Check if we have reached the end of the row
-                If col >= table.ColumnCount Then
-                    col = 0 ' Reset column
-                    row += 1 ' Move to the next row
-                End If
+                requestDataList.Add(requestData)
             End If
         End While
+        ' Sort the list based on distance (from nearest to furthest)
+        requestDataList = requestDataList.OrderBy(Function(data) data.Distance).ToList()
+
+        For Each requestData As RequestData In requestDataList
+            Dim customContainer As Panel = New Panel()
+            customContainer.BorderStyle = BorderStyle.FixedSingle
+            customContainer.Width = 300
+            customContainer.Height = 180
+            customContainer.BackColor = System.Drawing.Color.FromArgb(255, 255, 255)
+            customContainer.BorderStyle = BorderStyle.None
+
+            ' Set the column and row location of the custom container
+            table.SetColumn(customContainer, col)
+            table.SetRow(customContainer, row)
+
+            Dim titleLabel As Label = New Label()
+            titleLabel.Text = requestData.Title
+            titleLabel.Location = New Point(10, 10)
+            titleLabel.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+            titleLabel.Name = "lblTitle"
+            titleLabel.Size = New System.Drawing.Size(250, 20)
+            titleLabel.AutoEllipsis = True
+
+            Dim descriptionLabel As Label = New Label()
+            descriptionLabel.Text = "Description: " & requestData.Description
+            descriptionLabel.Location = New Point(10, 30)
+            descriptionLabel.AutoEllipsis = True
+            descriptionLabel.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+            descriptionLabel.Name = "lblDescription"
+            descriptionLabel.Size = New System.Drawing.Size(250, 60)
+
+            Dim paxLabel As Label = New Label()
+            paxLabel.Text = "Pax: " & requestData.Pax
+            paxLabel.Location = New Point(10, 90)
+            paxLabel.AutoSize = True
+            paxLabel.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+            paxLabel.Name = "lblPax"
+
+
+
+            Dim distanceLabel As Label = New Label()
+            distanceLabel.Text = "Distance: " & requestData.Distance.ToString("F2") & " km from you"
+            distanceLabel.Location = New Point(10, 110)
+            distanceLabel.AutoSize = True
+            distanceLabel.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+            distanceLabel.Name = "lblPax"
+
+
+
+            Dim button As KryptonButton = New KryptonButton()
+            button.Values.Text = "View"
+
+            button.Location = New System.Drawing.Point(185, 135)
+            button.Name = "btnView"
+            button.OverrideDefault.Back.Color1 = System.Drawing.Color.Gold
+            button.OverrideDefault.Back.Color2 = System.Drawing.Color.Gold
+            button.Size = New System.Drawing.Size(100, 30)
+            button.StateCommon.Back.Color1 = System.Drawing.Color.Gold
+            button.StateCommon.Back.Color2 = System.Drawing.Color.Gold
+            button.StateCommon.Border.DrawBorders = CType((((ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Top Or ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Bottom) _
+            Or ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Left) _
+            Or ComponentFactory.Krypton.Toolkit.PaletteDrawBorders.Right), ComponentFactory.Krypton.Toolkit.PaletteDrawBorders)
+            button.StateCommon.Border.Rounding = 10
+            button.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.Black
+            button.StateCommon.Content.ShortText.Color2 = System.Drawing.Color.Black
+            button.StateCommon.Content.ShortText.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+
+            'Get request ID and handles the view button
+            Dim requestID As Integer = requestData.RequestID
+            button.Tag = requestID
+            AddHandler button.Click, AddressOf button_Click
+
+
+            customContainer.Controls.Add(titleLabel)
+            customContainer.Controls.Add(descriptionLabel)
+            customContainer.Controls.Add(paxLabel)
+            customContainer.Controls.Add(distanceLabel)
+            customContainer.Controls.Add(button)
+
+
+            ' Add the custom container to the form or container control
+
+            table.Controls.Add(customContainer)
+
+            col += 1 ' Move to the next column
+
+            ' Check if we have reached the end of the row
+            If col >= table.ColumnCount Then
+                col = 0 ' Reset column
+                row += 1 ' Move to the next row
+            End If
+        Next
+
         pnlGridView.Controls.Clear() 'Clear Existing Control
         pnlGridView.Controls.Add(table)
     End Sub
